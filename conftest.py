c48360e6578a54960402e99144ca489958f6be2d
Artiom Chizh <artemciz993@gmail.com>
2026-04-17 01:59:54 +0300

    change conftest

--- conftest.py
+++ conftest.py
@@ -1,15 +1,8 @@
-# import pytest
-# from selene.support.shared import browser
-#
-#
-# @pytest.fixture(scope="function", autouse=True)
-# def open_browser():
-#     browser.config.base_url = 'https://www.adriver.ru/'
-#     yield
-#     browser.quit()
 import pytest
 from selene.support.shared import browser
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope="function", autouse=True)
@@ -19,10 +12,13 @@ def open_browser():
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--window-size=1920,1080")
 
-    browser.config.driver = webdriver.Chrome(options=chrome_options)
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+
+    browser.config.driver = driver
     browser.config.base_url = 'https://www.adriver.ru/'
 
     yield
     browser.quit()
+
