1297b1acac925c29b6ef833e94dc0f8578e332d9
Artiom Chizh <artemciz993@gmail.com>
2026-04-17 21:57:11 +0300

    change conftest

--- conftest.py
+++ conftest.py
@@ -1,24 +1,9 @@
 import pytest
 from selene.support.shared import browser
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
+
 
 @pytest.fixture(scope="function", autouse=True)
 def open_browser():
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=chrome_options)
-
-    browser.config.driver = driver
     browser.config.base_url = 'https://www.adriver.ru/'
-
     yield
-    browser.quit()
-
+browser.quit()
